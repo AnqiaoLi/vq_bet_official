065f5ec216fa846616f8a62327912284c92c5f0e
AnqiaoLi <3170104825@zju.edu.cn>
2024-08-15 15:03:06 +0200

    clean mock environment

--- examples/Mock_env.py
+++ examples/Mock_env.py
@@ -43,7 +43,7 @@ index_to_name_dict_add = {
 
 class MockEnv():
     """ Mock environment for rolling out the model"""
-    def __init__(self, cfg, data, num_env = 2, history_stat_index = 0, freeze_angle = False):
+    def __init__(self, cfg, data, num_env = 2, history_stat_index = 0, freeze_angle = False, repeat_init = True):
         self.num_env = num_env
         self.data = data.dataset.dataset.observations
         self.obs_dim = self.data.shape[-1]
@@ -51,21 +51,27 @@ class MockEnv():
         self.obs_length = cfg.model.obs_window_size
         self.history_stat_index = history_stat_index
         self.freeze_angle = freeze_angle
-        self.predict_contact = cfg.predict_contact
+        self.initialize_with
         self.device = cfg.device
-
+        
+        # init the input buffer for vq-bet input
+        # pick the environments
         self.env_indicies = torch.randperm(self.data.shape[0])[:num_env]
         # expend the env_indicies to the number of environments, if the dataset is too small
         while self.env_indicies.shape[0] < num_env:
             self.env_indicies = torch.cat([self.env_indicies, torch.randperm(self.data.shape[0])[:num_env - self.env_indicies.shape[0]]], dim = 0)
         self.init_state = self.data[self.env_indicies, self.history_stat_index:self.obs_length + self.history_stat_index]
+        # change the init input buffer to repeated states
+        if repeat_init:
+            self.init_state = self.init_state[:, 0].unsqueeze(1).repeat(1, self.obs_length, 1)
+        
         self.state = None
         self.reference_state = self.data[self.env_indicies, self.history_stat_index:]
         self.reference_add = data.dataset.dataset.actions[self.env_indicies, self.history_stat_index:, self.init_state.shape[-1]:]
         self.reset()
 
     ##########################################
-    # Functions for setting the initial state
+    # Functions for setting the initial oven angle
     def set_start_angle(self, start_state):
         """set start angle for the oven"""
         self.init_state[: ,:, -2 ] = start_state
@@ -87,7 +93,7 @@ class MockEnv():
         self.init_state[: ,:, -2 ] = angle_list
         self.reset()
     ##########################################
-
+    # Environment functions
     def reset(self):
         self.state = self.init_state
         self.state_list = torch.zeros((self.num_env, 0, self.data.shape[-1])).to(self.device)
@@ -101,13 +107,13 @@ class MockEnv():
             action: torch.tensor, shape (batch_size, action_dim)
             mode: str, "r" or "f", "r" for residual, "f" for full state
         """
-        # if action includes contact, remove it
+        # if action includes additional states, i.e. contact, remove it
         if action.shape[-1] > self.state.shape[-1]:
             add_state = action[:, :, self.state_list.shape[-1]:]
             action = action[:, :, :self.state.shape[-1]]
             self.add_list = torch.cat([self.add_list, add_state], dim = 1)
 
-        # Debug:keep the angle of the oven unchanged
+        # Debug: keep the angle of the oven unchanged
         if self.freeze_angle:
                 action[:, 0, -2] = self.state[:, -1, -2]
                 action[:, 0, -1] = 0
@@ -121,14 +127,22 @@ class MockEnv():
         # append the state and action to the container
         self.state_list = torch.cat([self.state_list, self.state[:, -1:]], dim = 1)
         self.action_list = torch.cat([self.action_list, action], dim = 1)
-        # if self.predict_contact:
-        #     self.contact_list = torch.cat([self.contact_list, contact], dim = 1)
 
     def get_obs(self):
         return self.state   
-
+    ##########################################
+    # Visualization
     def plot_different_state(self, plt_indicies = [6, 7, 8, 9, 10, 11], plt_time = 100, plot_env_num = 2, separate_env = False):
-        """ plot the difference between the state and the reference state in each environment"""
+        """ plot the difference between the state and the reference state in each environment
+            args:
+                plt_indicies: list, the index from index_to_name_dict_obs. 
+                                    If the index is larger than the max index in index_to_name_dict_obs, 
+                                    refer to the index_to_name_dict_add
+                plt_time: int, the time step to plot
+                plot_env_num: int, the number of environments to plot
+                separate_env: bool, if False, plot the states of different envs in the same figure, 
+                                    if True, plot the states and reference in different figures
+        """
         if separate_env:
             fig, axs = plt.subplots(plot_env_num, len(plt_indicies), figsize = (10*len(plt_indicies), 10))
             text_size = 20
@@ -147,6 +161,7 @@ class MockEnv():
                         axs[i, j].set_title(f"env {i}, {index_to_name_dict_add[add_state_index]}")
             # plt.show()
             plt.legend()
+            fig.suptitle("State and Reference Trajectories")
         else:
             fig, axs = plt.subplots(1, len(plt_indicies), figsize = (10*len(plt_indicies), 5))
             text_size = 20
@@ -161,15 +176,12 @@ class MockEnv():
                         add_state_index = plt_indicies[j] - self.state_list.shape[-1]
                         axs[j].scatter(range(self.add_list[i, :plt_time, add_state_index].shape[0]), self.add_list[i, :plt_time, add_state_index].cpu().detach().numpy(), linewidth = line_width)
                         axs[j].set_title(f"{index_to_name_dict_add[add_state_index]}")
-                    
-                    # axs[j].plot(self.state_list[i, :plt_time, plt_indicies[j]].cpu().detach().numpy(), linewidth = line_width)
-                    # # axs[j].plot(self.reference_state[i, :plt_time, plt_indicies[j]].cpu().detach().numpy(), label = "reference", linewidth = line_width)
-                    # axs[j].set_title(f"{index_to_name_dict_obs[plt_indicies[j]]}")
-
             # plt.show()
             plt.legend()
+            fig.suptitle("State Trajectories of {} Environments".format(plot_env_num))
         return fig
-    
+    ##########################################
+    # Save the data
     def save_data(self, path):
         save_params = {
             "state_list": self.state_list,
